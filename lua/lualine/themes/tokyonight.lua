9157396674981306afe1db096ed567a40b0002c5
Adri <carrasquilla.adria@gmail.com>
2022-12-27 21:15:21 +0100

    lualine colors

--- lua/lualine/themes/tokyonight.lua
+++ lua/lualine/themes/tokyonight.lua
@@ -4,40 +4,40 @@ local config = require("tokyonight.config").options
 local tokyonight = {}
 
 tokyonight.normal = {
-  a = { bg = colors.blue, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.blue },
+  a = { bg = colors.blue1, fg = colors.black, gui= "bold" },
+  b = { bg = colors.terminal_black, fg = colors.blue1 },
   c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
 }
 
 tokyonight.insert = {
-  a = { bg = colors.green, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.green },
+  a = { bg = colors.green, fg = colors.black , gui= "bold"},
+  b = { bg = colors.terminal_black, fg = colors.green },
 }
 
 tokyonight.command = {
-  a = { bg = colors.yellow, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.yellow },
+  a = { bg = colors.yellow, fg = colors.black , gui= "bold"},
+  b = { bg = colors.terminal_black, fg = colors.yellow1 },
 }
 
 tokyonight.visual = {
-  a = { bg = colors.magenta, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.magenta },
+  a = { bg = colors.magenta, fg = colors.black, gui= "bold" },
+  b = { bg = colors.terminal_black, fg = colors.magenta },
 }
 
 tokyonight.replace = {
-  a = { bg = colors.red, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.red },
+  a = { bg = colors.red, fg = colors.black, gui= "bold" },
+  b = { bg = colors.terminal_black, fg = colors.red },
 }
 
 tokyonight.terminal = {
-  a = {bg = colors.green1, fg = colors.black },
-  b = {bg = colors.fg_gutter, fg=colors.green1 },
+  a = {bg = colors.green1, fg = colors.black, gui= "bold" },
+  b = {bg = colors.terminal_black, fg=colors.green1 },
 }
 
 tokyonight.inactive = {
-  a = { bg = colors.bg_statusline, fg = colors.blue },
-  b = { bg = colors.bg_statusline, fg = colors.fg_gutter, gui = "bold" },
-  c = { bg = colors.bg_statusline, fg = colors.fg_gutter },
+  a = { bg = colors.bg, fg = colors.blue, gui= "bold" },
+  b = { bg = colors.bg, fg = colors.fg_gutter, gui = "bold" },
+  c = { bg = colors.bg, fg = colors.fg_gutter },
 }
 
 if config.lualine_bold then
